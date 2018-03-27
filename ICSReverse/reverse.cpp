8b6965ad87730b00703fba894e335cafbcf30f32
Jean Bond <xushouwei@163.com>
2018-03-27 22:00:04 +0800

    cpp中int/char/string相互转换
    1、int/string/char*/char[] 之间可以使用stringstream作为中间桥梁相互转换；
    2、单字节如char/int/之间可以直接使用asicc码表的大小直接运算得到；

--- ICSReverse/reverse.cpp
+++ ICSReverse/reverse.cpp
@@ -1,17 +1,51 @@
 using namespace std;
 
 #include <string>
+#include <sstream>
 
 class ReverseICS 
 {
 	/* reverse int-char-string */
 public:
 	int int2char(int &in, char &out) {
-		out = in + ' ';
+		out = in + '0';
 		return 0;
+	}
+	int char2int(char &in, int &out) {
+		out = in - '0';
+		return 0;
+	}
+
+	int str2charp(string &in, char *out) {
+		if (0) {
+			strcpy_s(out, in.size(), in.c_str());
+		}
+		else {
+			stringstream ss;
+			ss << in;
+			ss >> out;
+		}
+		return 0;
+	}
+
+	int str2char(string &in, char &out) {
+		stringstream ss;
+		ss << in;
+		ss >> out;
+		return 0;
+	}
+
+	int charp2str(char *in, string &out) {
+		out = in;
+		return 0;	
 	}
 };
 
 int main() {
+	ReverseICS re = ReverseICS();
+	int i = 8;
+	char c ;
+	string s = { "hello" };
+	re.str2char(s, c);
 	return 0;
 }
