f24bf5726e2254c957e45f16d5bc04f195e6daf9
xuwei <xushouwei@163.com>
2018-03-28 11:49:40 +0800

    二叉树序列化注释上库
    上库详细注释

--- BinaryTreeSerialization/code.cpp
+++ BinaryTreeSerialization/code.cpp
@@ -2,6 +2,7 @@ using namespace std;
 
 #include<string>
 #include<queue>
+#include<sstream>
 
 class TreeNode {
 public:
@@ -12,6 +13,14 @@ public:
          this->val = val;
          this->left = this->right = NULL;
      }
+	 TreeNode(string val) {
+		 stringstream ss;
+		 ss << val;
+		 int va = 0;
+		 ss >> va;
+		 this->val = va;
+		 this->left = this->right = NULL;
+	 }
 };
 
 
@@ -31,15 +40,19 @@ public:
 	}
 private:
 	void serials(TreeNode *node, string &str) {
+		//when we use a recursion func,
+		//we must have at least one return path.
 		if (node) {
 			str += (to_string(node->val) + ",");
 			serials(node->left, str);
 			serials(node->right, str);
 		}
 		else {
-			str += "#";
+			str += "#,";
 			return;
 		}
+		// clean the last , mark in string list.
+		str[str.size() - 1] = '\0';
 		return;
 	}
 
@@ -53,12 +66,14 @@ public:
 	* "serialize" method.
 	*/
 	TreeNode * deserialize(string &data) {
+		// to identify the global location in string data.
 		int pos = 0;
 		return deTree(data, pos);
 	}
 
 	TreeNode* deTree(string &data, int &pos) {
 
+		//operate the special treatment.
 		if (data[pos] == ',') { pos++; }
 		if (data[pos] == '\0') { return NULL; }
 
@@ -67,12 +82,17 @@ public:
 			return NULL;
 		}		
 
+		// calculate multibit data.
+		//caution: convert char to int number.
 		int nownum = 0;
 		while (data[pos] != ',' && data[pos]!= '\0') {
 			nownum = (nownum * 10 ) + (data[pos] - '0');
 			pos++;
 		}
 
+		// this recursion seems some special.
+		// no return swith, use the terminal return to end it;
+		// so we must have a retrun at the end;
 		TreeNode *node = new TreeNode(nownum);
 		node->left = deTree(data, pos);
 		node->right = deTree(data, pos);
@@ -81,6 +101,7 @@ public:
 
 };
 
+/* someting wrong with this Solution2 class */
 class Solution2
 {
 	/* use BFS to solve */
@@ -88,22 +109,48 @@ private :
 	queue<int> que;
 
 public:
-	unsigned int serializtation(string &src) {
+	TreeNode* serializtation(string &src) {
+		int rc = 0;
 		if (src.size() <= 0) {
-			return -1;
+			return NULL;
 		}
 		vector<string> nodelist;
-		parseString(src, nodelist);
-		return 0;
+		rc = parseString(src, nodelist);
+		if (rc) { return NULL; }
+
+		return serialTree(nodelist);
 	}
 
 private:
+	/* 二叉树的非递归构造方法实现：
+		该函数有问题，未能实现功能，如果想测试代码，请直接使用Solution类解决。
+		Solution类已经通过lc测试，没问题； */
+	TreeNode * serialTree(vector<string> &in) {
+		TreeNode *root = new TreeNode(in[0]) , *pstPre = NULL;
+		queue<string> qu;
+		pstPre = root;
+		int pos = 0;
+
+		qu.push(in[pos]);
+	
+		while (!qu.empty()) {
+			int len = qu.size();
+			for (int i = 0; i < len; i++) {
+				if (in[pos + 1] != "#") { qu.push(in[pos + 1]); }
+				if (in[pos + 2] != "#") { qu.push(in[pos + 2]); }
+			}
+
+			pos++;
+		}
+		return root;
+	}
+
 	/* parse all information into vector without determin char:',' */
 	unsigned int parseString(string &input, vector<string> &output) {
 		if (input.size() <= 0) { return 0; }
 		int pos = 0, level = 1;
 
-		while (pos < input.size() && input[pos] != '\0') {			
+		while (pos < (int)input.size() && input[pos] != '\0') {			
 			unsigned int elemCnt = 0;
 			for (int i = 0; i < level && input[pos] != '\0'; i++) {
 				string stmp="";
